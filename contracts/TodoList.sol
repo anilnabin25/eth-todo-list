095e1c85c42b3136118343169bd9837fb74fe3b3
Divya Shrestha <53654225+anilnabin25@users.noreply.github.com>
2021-08-30 11:52:20 +0545

    fixing the truffel and todolist added

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,1 +1,22 @@
-preg
+pragma solidity ^0.5.0;
+
+contract TodoList {
+  uint public taskCount = 0;
+
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
+
+  mapping(uint => Task) public tasks;
+  constructor() public {
+    createTask("Check the data");
+  }
+
+  function createTask(string memory _content) public {
+    taskCount ++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+  }
+}
+
