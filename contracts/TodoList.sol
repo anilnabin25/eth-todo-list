f686dd43f1c52cedd990a506dc63a46ac2648817
Divya Shrestha <53654225+anilnabin25@users.noreply.github.com>
2021-08-31 10:03:47 +0545

    update  and fixed the constructor

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -9,6 +9,18 @@ contract TodoList {
     bool completed;
   }
 
+  event TaskCreated(
+    uint id,
+    string content,
+    bool completed
+  );
+
+  event TaskCompleted(
+    uint id,
+    bool completed
+  );
+
+
   mapping(uint => Task) public tasks;
   constructor() public {
     createTask("Check the data");
@@ -17,6 +29,14 @@ contract TodoList {
   function createTask(string memory _content) public {
     taskCount ++;
     tasks[taskCount] = Task(taskCount, _content, false);
+    emit TaskCreated(taskCount, _content, false);
+  }
+
+  function toggleCompleted(uint _id) public {
+    Task memory _task = tasks[_id];
+    _task.completed = !_task.completed;
+    tasks[_id] = _task;
+    emit TaskCompleted(_id, _task.completed);
   }
 }
 
